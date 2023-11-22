b4cba05b0aa48921f0ffb92b8f9484eea8e98d44
Maurílio <mauriliojose249@gmail.com>
2023-11-21 23:14:15 -0300

    corrigindo nome da classe IntegraPricesApplication

--- app/src/main/java/com/mentoria/integraprices/IntegraPricesApplication.java
+++ app/src/main/java/com/mentoria/integraprices/IntegraPricesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntegrapricesApplication {
+public class IntegraPricesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IntegrapricesApplication.class, args);
+		SpringApplication.run(IntegraPricesApplication.class, args);
 	}
 
 }
